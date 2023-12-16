f53de73fd414370217d0eace2922b3f147d862db
GardevoirX <xqj.2001@outlook.com>
2023-12-16 16:34:05 +0100

    notebook and distance updatte

--- utils/energy_util.py
+++ utils/energy_util.py
@@ -1,14 +1,47 @@
+import ase
 import numpy as np
 from scipy.spatial import distance_matrix
 
+L = 40  # Å
+CELL = L * np.eye(3)
+
+def dist_pbc(pos1, pos2):
+    """
+    Calculate the distance between two points in a periodic boundary condition system.
+
+    Parameters:
+        pos1 (numpy.ndarray): The coordinates of the first point.
+        pos2 (numpy.ndarray): The coordinates of the second point.
+
+    Returns:
+        float: The distance between the two points.
+    """
+    return ase.geometry.get_distances(pos1, pos2, cell=CELL, pbc=True)[1]
+
+def calc_pair_distances(atoms: ase.Atoms):
+    """
+    Calculate the distances between pairs of atoms in an ASE Atoms object.
+
+    Parameters:
+        atoms (ase.Atoms): The ASE Atoms object containing the atoms.
+
+    Returns:
+        numpy.ndarray: An array containing the distances between pairs of atoms.
+    """
+
+    dist = []
+    for i, atom_i in enumerate(atoms[:-1]):
+        for atom_j in atoms[i+1:]:
+            dist.append(dist_pbc(atom_i.position, atom_j.position)[0])
+
+    return np.array(dist)
 
 def calc_dimer_trimer_energies(frames, energy_inter):
     dimer_trimer_energies = np.zeros(len(frames))
 
     for i, atoms in enumerate(frames):
-        dist_mat = distance_matrix(atoms.positions, atoms.positions)
+        pair_distances = calc_pair_distances(atoms)
         # print(dist_mat)
-        pair_distances = dist_mat[np.triu_indices(len(atoms), k=1)]
         # if np.isnan(pair_distances):
         pred_dimer = energy_inter(pair_distances)
         # print(pred_dimer)
